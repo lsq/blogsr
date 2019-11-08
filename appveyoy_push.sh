1eb6e9c560adcb2825ec246017b2b69b98337d03
lsq <lsqypj@gmail.com>
2019-11-08 14:39:42 +0800

    add update push script

--- appveyoy_push.sh
+++ appveyoy_push.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 grep "nothing to commit" <(git status) && exit 0
 pwd
+git status
 git add -A
 git config -l
 git commit -m "Update Static Site"
-echo %TARGET_BRANCH%
-git push -u origin %TARGET_BRANCH%
+env
+declare
+echo $TARGET_BRANCH
+git push -u origin $TARGET_BRANCH
