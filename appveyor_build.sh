4a013ed1802c7b5b26fb39285c93a4b664f5103b
lsq <lsqypj@gmail.com>
2019-11-09 22:10:59 +0800

    update build script

--- appveyor_build.sh
+++ appveyor_build.sh
@@ -2,5 +2,5 @@
 
 eval $(sed -n '/theme/s/ //gp' config.toml)
 echo $theme
-which hugo && /usr/bin/hugo -t $theme # --baseUrl="https://github.com/lsq/lsq.github.io"
+which hugo && /usr/bin/hugo -t $theme  --baseUrl="https://github.com/lsq/lsq.github.io"
 
