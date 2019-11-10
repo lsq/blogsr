48431fe090c74c9cd5f73802bdf3aa3b7bdae50a
lsq <lsqypj@gmail.com>
2019-11-10 15:11:16 +0800

    use file list download

--- get_latest.sh
+++ get_latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -xe
+set -xuo pipefail
 
 function get_latest_release() {
   curl --silent "https://api.github.com/repos/${1}/releases/latest" | # Get latest release from GitHub api
@@ -10,13 +10,18 @@ function get_latest_release() {
 
 }
 function get_release(){
-  file_list=`curl --silent "https://github.com/${1}/releases" |
+  local file_list=`curl --silent "https://github.com/${1}/releases" |
     sed  -n 's^.*a href="\('"/${1}"'/releases/download/\)\([^"]*\)".*^\1\2^p'`
   [[ -n $file_list ]] && echo "found files: $file_list"
   #pathd=$(grep $2 <<<"$file_list")
-  pathd=$(echo "$file_list" | grep $2)
-  [[ -n $pathd ]] && echo -e "Downloading ...........................\n" && curl -sOL "https://github.com$pathd" && echo "--------------finished!--------------------"
+  local pathd=$(echo "$file_list" | grep "$2")
+  [[ -n $pathd ]] && echo -e "Begin downloading ...........................\n" && curl -sOL "https://github.com$pathd" && echo "--------------finished!--------------------"
 }
-[[ -n $(get_latest_release $1) ]] && echo "latest release is: $(get_latest_release $1)"
-get_release $1 $2
-cp -f $2 src/
+# [[ -n $(get_latest_release $1) ]] && echo "latest release is: $(get_latest_release $1)"
+while read -r line; do 
+  user_repo=$(echo "$line" | gawk '/^([[:space:]]*)[^ #]/ {print $1}')
+  dl_filename=$(echo "$line" | gawk '/^([[:space:]]*)[^ #]/ {print $2}')
+  get_release "$user_repo" "$dl_filename"
+  [ $? -eq 0 ] && mv -f "$dl_filename" $APPVEYOR_JOB_ID/ && sed -i '/'"$dl_filename"'/s/^/###dl /' $1
+done <$1
+cp -rf $1 content/posts/downloaded.md
